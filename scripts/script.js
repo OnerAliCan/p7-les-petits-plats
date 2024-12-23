3ddc8014dd2b608b5f66b518cbd1348faeb53977
Ali <alican.oner.pro@gmail.com>
2024-12-23 17:42:39 +0100

    ingredients filtering wip, 23/12 17h42

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+function dropdownShow() {
+	document.getElementById("dropdown-items").classList.toggle("show");
+	// console.log(typeof recipes);
+	let ingredientNameArray = [];
+
+	recipes.forEach((recipe) => {
+		const recipeIngredients = recipe.ingredients;
+
+		recipeIngredients.forEach((IngredientObject) => {
+			const ingredientName = IngredientObject.ingredient;
+			//ajouter la fonction qui enlève les doublons
+			ingredientNameArray.push(ingredientName);
+			// console.log(ingredientNameArray);
+		});
+	});
+
+	const uniqueIngredientNameArray = [
+		...new Set(
+			ingredientNameArray.map((ingredient) =>
+				ingredient.trim().toLowerCase()
+			)
+		),
+	];
+
+	const formattedIngredientArray = uniqueIngredientNameArray
+		.map(
+			(ingredient) =>
+				ingredient.charAt(0).toUpperCase() + ingredient.slice(1)
+		)
+		.sort();
+
+	formattedIngredientArray.forEach((ingredient) => {
+		const itemDropdown = document.createElement("a");
+		const dropdownItems = document.getElementById("dropdown-items");
+		itemDropdown.classList.add("item-dropdown");
+		itemDropdown.innerText = ingredient;
+		dropdownItems.appendChild(itemDropdown);
+		itemDropdown.addEventListener("click", () => saveSelect(ingredient));
+	});
+
+	// ["lait de coco", "lait de vache"]
+
+	document
+		.getElementById("dropdown-button")
+		.classList.toggle("border-radius-bottom");
+}
+
+function filterFunction() {
+	const input = document.getElementById("dropdown-search");
+	const filter = input.value.toUpperCase();
+	const div = document.getElementById("dropdown-items");
+	const a = div.getElementsByTagName("a");
+	for (let i = 0; i < a.length; i++) {
+		txtValue = a[i].textContent || a[i].innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			a[i].style.display = "";
+		} else {
+			a[i].style.display = "none";
+		}
+	}
+}
+
+function clearInput() {
+	const input = document.getElementById("dropdown-search");
+	input.value = "";
+	input.focus();
+	filterFunction(); // Remet le focus sur le champ input
+}
+function saveSelect(ingredient) {
+	console.log(ingredient);
+}
