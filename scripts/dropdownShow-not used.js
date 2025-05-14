25d87e0d62e0b1f9d9e30f84e9c6a51866304e60
unknown <alican.oner.pro@gmail.com>
2025-05-14 18:58:30 +0200

    avant soutenance

--- scripts/dropdownShow-not used.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-function dropdownShow() {
-	document.getElementById("dropdown-items").classList.toggle("show");
-
-	document
-		.getElementById("dropdown-button")
-		.classList.toggle("border-radius-bottom");
-}
